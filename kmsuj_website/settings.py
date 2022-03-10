9f7913dad53bfc8c523960aed8f6f9a4f0442d8c
janczarknurek <jacek@beit.tech>
2022-03-10 21:59:10 +0100

    Add volume for database
    Signed-off-by: janczarknurek <jacek@beit.tech>

--- kmsuj_website/settings.py
+++ kmsuj_website/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'kmsuj_website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/db/db.sqlite3',
     }
 }
 
